3aca66b2f5cf33e1efbeaa07ac9dc731dee87cde
Fuvolution <ams.dinhphuc@gmail.com>
2023-09-19 19:25:39 -0500

    Javascript file changes in response to API swap

--- script.js
+++ script.js
@@ -20,36 +20,6 @@ function fetchCityStateCountry(zipCode) {
       });
   }
 
-  // Function to fetch a random activity from the Bored API, display it along with city, state, and country
-  function fetchRandomActivityAndDisplay() {
-    const zipCode = document.getElementById('search-input').value;
-
-    // Make a GET request to the Bored API
-    fetch('https://www.boredapi.com/api/activity')
-      .then((response) => response.json())
-      .then((activityData) => {
-        // Fetch city, state, and country information based on the zip code
-        fetchCityStateCountry(zipCode)
-          .then(({ city, state, country }) => {
-            // Get the ul element by its id
-            const resultsList = document.getElementById('results');
-
-            // Create a new li element to display the random activity, city, state, and country
-            const newActivityItem = document.createElement('li');
-            newActivityItem.textContent = `${activityData.activity} in ${city}, ${state}, ${country}`;
-
-            // Append the li element to the ul
-            resultsList.appendChild(newActivityItem);
-          })
-          .catch((error) => {
-            console.error('Error fetching city, state, and country information:', error);
-          });
-      })
-      .catch((error) => {
-        console.error('Error fetching random activity:', error);
-      });
-  }
-
   // Event listener for the "Tell me what to do!" button
   document.getElementById('search-button').addEventListener('click', function () {
     // Call the function to fetch and display a random activity with city, state, and country
@@ -65,44 +35,12 @@ function fetchCityStateCountry(zipCode) {
 
   */
 
-  
-
-var zipCode = document.querySelector("#zip-code");
-var sortBy = document.querySelector("#criteria");
-
-
-
 //test api call for zippopotum.us
 //currently console logs all zip code info when zip code is entered in input form
-function getZip(event){
 
-    event.preventDefault();
-    var code = document.getElementById("search-input").value;
-    var client = new XMLHttpRequest();
-    client.open("GET", "http://api.zippopotam.us/us/" + code, true);
-    client.onreadystatechange = function() {
-        if(client.readyState == 4) {
-            console.log(client.responseText);
-        };
-    };
-    client.send();
-}
-
-
-function findEvents(event){
-    event.preventDefault();
-
-    fetch("https://www.boredapi.com/api/activity/")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-    })
-};
-
-function getEvents() {
-    const url = 'https://real-time-events-search.p.rapidapi.com/search-events?query=sports%20in%20Austin&start=0';
+// Two variables needed for this function: city and event
+function getEvents(city, event) {
+    const url = 'https://real-time-events-search.p.rapidapi.com/search-events?query=' + event + '%20in%20' + city + '&start=0';
     const options = {
         method: 'GET',
         headers: {
